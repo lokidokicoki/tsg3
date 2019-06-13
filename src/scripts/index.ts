f761a765d3b2960738294438e33bccca80a23a19
lokidokicoki <lokidokicoki@gmail.com>
2019-06-13 21:43:09 +0100

    feat: controls for move and rotate. vectors not working properly, my maths is bad :(

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -8,11 +8,28 @@ const things: Set<Thing> = new Set();
 
 Renderer.getInstance(`arena`);
 function run(): void {
-  document.getElementById(`move`).addEventListener(`click`, move);
+  document.getElementById(`move`).addEventListener(`click`, () => { move(null) });
   document.getElementById(`rotate`).addEventListener(`click`, rotate);
+  document.getElementById(`rotzero`).addEventListener(`click`, rotzero);
+  document.getElementById(`update`).addEventListener(`click`, update);
+  document.getElementById(`down`).addEventListener(`click`, () => {
+    move({ y: 10 });
+  });
+  document.getElementById(`up`).addEventListener(`click`, () => {
+    move({ y: -10 });
+  });
+  document.getElementById(`right`).addEventListener(`click`, () => {
+    move({ x: 10 });
+  });
+  document.getElementById(`left`).addEventListener(`click`, () => {
+    move({ x: -10 });
+  });
+  document.getElementById(`center`).addEventListener(`click`, () => {
+    move({ x: 250, y: 250, center: true });
+  });
 
-  for (let i = 0; i < 5; i++) {
-    things.add(new Thing(getRandomInt(500), getRandomInt(500), getRandomInt(360)));
+  for (let i = 0; i < 1; i++) {
+    things.add(new Thing(getRandomInt(500), getRandomInt(500), 0));//getRandomInt(360)));
   }
 }
 
@@ -20,18 +37,56 @@ function getRandomInt(max: number) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function move() {
+function move(opts?: any) {
   things.forEach((thing: Thing) => {
-    thing.position = { x: getRandomInt(500), y: getRandomInt(500) };
-    thing.update();
+    if (opts) {
+
+
+      if (opts.center) {
+        thing.position = { x: opts.x, y: opts.y }
+        thing.update();
+      } else if (opts.x || opts.y) {
+        thing.position = { x: thing.position.x + opts.x || thing.position.x, y: thing.position.y + opts.y || thing.position.y };
+        thing.update();
+      }
+    } else {
+      thing.move()
+    }
   });
 }
 function rotate() {
   things.forEach((thing: Thing) => {
-    thing.direction = getRandomInt(360);
+    thing.direction += 5;
     thing.update();
   });
 }
+function rotzero() {
+  things.forEach((thing: Thing) => {
+    thing.direction = 0;
+
+    thing.update(true);
+  });
+}
+function plusX() {
+  things.forEach((thing: Thing) => {
+    thing.position.x += 10;
+
+    thing.update();
+  });
+}
+function plusY() {
+  things.forEach((thing: Thing) => {
+    thing.position.y += 10;
+
+    thing.update();
+  });
+}
+function update() {
+  things.forEach((thing: Thing) => {
+    thing.update();
+  });
+}
+
 
 
 document.addEventListener(`DOMContentLoaded`, run);
--- src/scripts/thing.ts
+++ src/scripts/thing.ts
@@ -5,9 +5,13 @@ export default class Thing {
   public health: number;
   public position: { x: number, y: number };
   public id: number;
-  public direction: number;
   public speed: number;
   public gfx: Renderable;
+  private angle: number;
+  private dx: number;
+  private dy: number;
+  private dirty: boolean;
+  private halfPi: number;
 
   /**
    * Creates an instance of Thing.
@@ -17,19 +21,51 @@ export default class Thing {
     this.health = 100;
     this.position = { x, y };
     this.id = 1;
-    this.direction = d;
-    this.speed = 10;
+    this.angle = d;
+    this.dirty = true;
+    this.speed = 0;
+    this.halfPi = (Math.PI / 180);
     this.createGfx();
   }
 
+  public set direction(angle: number) {
+    this.dirty = true;
+    this.angle = angle;
+  }
+  public get direction(): number {
+    return this.angle;
+  }
+
+  public move() {
+    this.dirty = true;
+    this.speed = 10;
+    this.update();
+  }
   /**
    * Update gfx
    *
    * @memberof Thing
    */
-  public update() {
-    console.log(this.position, this.direction);
-    this.gfx.prop(`transform`, `translate(${this.position.x}, ${this.position.y}) rotate(${this.direction}, 15,15)`);
+  public update(ignoreMove?: boolean) {
+
+    if (this.angle > 360) {
+      this.angle -= 360
+    }
+
+    if (this.dirty) {
+      this.dx = this.speed * Math.cos(this.angle / this.halfPi)
+      this.dy = this.speed * Math.sin(this.angle / this.halfPi)
+      this.dirty = false;
+    }
+
+    if (ignoreMove === undefined) {
+      this.position.x += this.dx;
+      this.position.y += this.dy;
+    }
+    this.speed = 0;
+
+    console.log(this.dx, this.dy, this.position, this.angle);
+    this.gfx.prop(`transform`, `translate(${this.position.x}, ${this.position.y}) rotate(${this.angle}, 15,15)`);
   }
 
   /**
@@ -39,11 +75,13 @@ export default class Thing {
    * @memberof Thing
    */
   private createGfx() {
-    this.gfx = new Renderable(`g`, { transform: `translate(${this.position.x}, ${this.position.y}) rotate(${this.direction}, 15, 15)` });
+    this.gfx = new Renderable(`g`, { transform: `translate(${this.position.x}, ${this.position.y}) rotate(${this.angle}, 15, 15)` });
     const body = new Renderable(`rect`, { width: 30, height: 30, fill: `#00ff00`, x: 0, y: 0 });
-    const eye = new Renderable(`rect`, { width: 10, height: 10, fill: `#000000`, x: 10, y: 0 });
+    const eye = new Renderable(`rect`, { width: 10, height: 10, fill: `#000000`, x: 20, y: 10 });
+    // const guide = new Renderable(`line`, { x1: 0, y1: 0, stroke: `black`, x2: 60, y2: 0 });
     this.gfx.append(body);
     this.gfx.append(eye);
+    // this.gfx.append(guide);
 
     Renderer.getInstance().container.append(this.gfx);
   }
